71c39e638dce5ccaf005ddd87faa6b092c42d07a
David Wu <david204code@gmail.com>
2021-03-04 23:10:19 +0000

    add the condition to replace the html text

--- app.js
+++ app.js
@@ -17,5 +17,15 @@ btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     // console.log(e.currentTarget);
     const style = e.currentTarget.classList;
+    if (style.contains("decrease")) {
+      count--;
+    } else if (style.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    // console.log(count);
+    // replace the text counter
+    value.textContent = count;
   });
 });
