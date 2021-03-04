9108d493df211c5547c11ca1bb76e5f621c2c8e6
David Wu <david204code@gmail.com>
2021-03-04 22:59:48 +0000

    set up initial count value, selecting html for access and using forEach function

---
+++ app.js
@@ -0,0 +1,15 @@
+// set up initial count value
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+// console.log(value);
+
+const btns = document.querySelectorAll(".btn");
+// ^^^ get nodeList
+// console.log(btns);
+
+// access each any every button
+btns.forEach(function (btn) {
+  // console.log(btn);
+});
